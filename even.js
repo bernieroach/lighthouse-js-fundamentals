e4f521782b4ef000fe78fc87b5c9e9193287a274
Bernard Roach <bernieroach@gmail.com>
2017-09-20 14:00:42 +0000

    Adds function exercises

---
+++ even.js
@@ -0,0 +1,7 @@
+function isEven(x){
+  return x % 2 === 0;
+}
+
+
+console.log(isEven(10));
+console.log(isEven(11));
---
+++ hello.js
@@ -0,0 +1,4 @@
+function sayHello(name){
+  console.log("Hello World " + name);
+}
+sayHello('Bernie');
